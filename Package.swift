6fbbeca8d13bee60da605ff1e41a66b7512759fd
Vlad <Suhomlinov.dev@gmail.com>
2020-08-09 13:33:03 +0300

    Add spm

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+  name: "UIScrollView_InfiniteScroll",
+  platforms: [
+      .iOS(.v10)
+  ],
+  products: [
+    .library(name: "UIScrollView_InfiniteScroll", targets: ["UIScrollView_InfiniteScroll"])
+  ],
+  targets: [
+    .target(name: "UIScrollView_InfiniteScroll", path: "Classes")
+  ]
+)
